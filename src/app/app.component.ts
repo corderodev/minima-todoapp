353c1054b04c03052f51726e17d4d28c5378034a
Daniel Cordero <daniel@corderodev.com>
2024-07-09 11:40:41 -0400

    feat: add home page with todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,51 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'minima-todoapp';
+  todos: { task: string, done: boolean }[] = [];
+  newTodo: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.loadTodos();
+  }
+
+  addTodo() {
+    if (this.newTodo.trim()) {
+      this.todos.push({ task: this.newTodo.trim(), done: false });
+      this.newTodo = '';
+      this.saveTodos();
+    }
+  }
+
+  toggleDone(index: number) {
+    this.todos[index].done = !this.todos[index].done;
+    this.saveTodos();
+  }
+
+  deleteTodo(index: number) {
+    this.todos.splice(index, 1);
+    this.saveTodos();
+  }
+
+  saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
+
+  loadTodos() {
+    const savedTodos = localStorage.getItem('todos');
+    if (savedTodos) {
+      this.todos = JSON.parse(savedTodos);
+    }
+  }
 }
